82c6e5369cdc81b016c7971f8980580365527bcb
Nathan Campos <eeepc904@gmail.com>
2020-10-23 14:39:32 +0100

    Added a function to package up Gerbers into compressed archives for manufacturing

--- Modules/CadsoftEagleUtilities/CadsoftEagleUtilities.psm1
+++ Modules/CadsoftEagleUtilities/CadsoftEagleUtilities.psm1
@@ -25,13 +25,71 @@ Remove-Item
 #>
 function Remove-EagleBackupFiles {
     [CmdletBinding()]
-    param(
+    Param(
         [Parameter(ValueFromPipeline, Position = 0, Mandatory = $true)]
         [String[]]$Path
     )
 
     # Get all the files with extensions like .s#1, .b#2, and deletes them.
     Get-ChildItem $Path\* | 
-        Where-Object { $_.FullName -match "\.\w#\d$" } |
+        Where-Object { $_.FullName -Match "\.\w#\d$" } |
         Remove-Item
+}
+
+<#
+.SYNOPSIS
+Builds a Gerber ZIP package for distribution and uploading to PCB manufacturers.
+
+.DESCRIPTION
+Builds a Gerber ZIP package for distribution and uploading to PCB manufacturers.
+
+.PARAMETER Path
+Path of the directory where the exported Gerbers reside in.
+
+.PARAMETER Name
+Name of the project. This will be the name of the ZIP file after it's packaged.
+
+.PARAMETER DestinationPath
+Path to where the ZIP file with the Gerbers will be placed.
+
+.PARAMETER Clean
+Deletes the Gerbers that were compressed into an archive.
+
+.INPUTS
+None.
+
+.OUTPUTS
+Nothing.
+
+.LINK
+Compress-Archive
+#>
+function Compress-Gerbers {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, Mandatory = $true)]
+        [String]$Path,
+        [Parameter(Position = 1, Mandatory = $true)]
+        [String]$Name,
+        [Parameter(Position = 2, Mandatory = $false)]
+        [String]$DestinationPath = (Get-Item $Path).FullName,
+        [Parameter(Position = 3, Mandatory = $false)]
+        [Switch]$Clean
+    )
+
+    # Get the path to the current directory.
+    $Path = (Get-Item $Path).FullName
+    $DestinationFile = Join-Path -Path (Get-Item $DestinationPath).FullName -ChildPath "$Name.zip"
+
+    # Get our precious gerbers.
+    $Gerbers = Get-Item $Path\* |
+               Where-Object { $_.FullName -Match "\.(g[bmpt]\w)|(dri)|(txt)$" }
+
+    # Compress the package for manufacturing.
+    Compress-Archive -Path $Gerbers -DestinationPath $DestinationFile -Force
+
+    # Clean up the unpackaged Gerbers.
+    If ($Clean -eq $true) {
+        Remove-Item $Gerbers
+    }
 }
