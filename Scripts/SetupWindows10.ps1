39ec754d5648c07b3f5095e2fb2b5febc2f01381
Nathan Campos <eeepc904@gmail.com>
2020-10-27 11:46:18 +0000

    Started working on a Windows 10 setup/debloat script

---
+++ Scripts/SetupWindows10.ps1
@@ -0,0 +1,327 @@
+﻿# SetupWindows10.ps1
+# My script to debloat and setup Windows 10 on new installs.
+# Based on the work of: Disassembler <disassembler@dasm.cz>
+#
+# Author: Nathan Campos <nathan@innoveworkshop.com>
+
+
+<#
+.SYNOPSIS
+Comfirms an action with the user.
+
+.PARAMETER Message
+Message that will be shown for the user to accept or reject.
+
+.OUTPUTS
+True if the user confirmed the action.
+#>
+Function Confirm-WithUser {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String]$Message
+    )
+
+    $Response = Read-Host -Prompt "$Message [y/n]"
+    While ($Response -NotMatch "[YyNn]") {
+        $Response = Read-Host -Prompt "$Message [y/n]"
+    }
+
+    Return $Response -Match "[Yy]"
+}
+
+<#
+.SYNOPSIS
+Confirms if the user wants to disable something.
+
+.PARAMETER FeatureName
+Name of the functionality to disable.
+
+.OUTPUTS
+True if the user *doesn't* want to disable the functionality.
+
+.EXAMPLE
+If (Confirm-DisableWithUser "telemetry") {
+    Return
+}
+#>
+Function Confirm-DisableWithUser {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String]$FeatureName
+    )
+    Return -Not (Confirm-WithUser "Do you want to disable $($FeatureName)?")
+}
+
+<#
+.SYNOPSIS
+A little welcome message and a confirmation with the user to begin the process.
+#>
+Function Write-Introduction {
+    Write-Output "==============================================="
+    Write-Output "===== Windows 10 Setup and Debloat Script ====="
+    Write-Output "=====          by Nathan Campos           ====="
+    Write-Output "==============================================="
+    Write-Output ""
+    Write-Output "This script will guide you through the setup and debloating of your system."
+
+    If (-Not (Confirm-WithUser "Shall we begin?")) {
+        Exit
+    }
+
+    Write-Output ""
+}
+
+# Disable Telemetry
+# Note: This tweak may cause Enterprise edition to stop receiving Windows updates.
+# Windows Update control panel will then show message "Your device is at risk because it's out of date and missing important security and quality updates. Let's get you back on track so Windows can run more securely. Select this button to get going".
+# In such case, enable telemetry, run Windows update and then disable telemetry again. See also https://github.com/Disassembler0/Win10-Initial-Setup-Script/issues/57
+Function Disable-Telemetry {
+    Write-Output "May cause Enterprise edition to stop receiving Windows updates."
+    If (Confirm-DisableWithUser "telemetry") {
+        Return
+    }
+
+	Write-Output "Disabling Telemetry..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\ProgramDataUpdater" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Autochk\Proxy" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" | Out-Null
+}
+
+# Disable WiFi Sense, just a crowdsourcing thing to know which hotspots to connect to.
+Function Disable-WiFiSense {
+    If (Confirm-DisableWithUser "WiFi Sense") {
+        Return
+    }
+
+	Write-Output "Disabling Wi-Fi Sense..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name "Value" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Name "AutoConnectAllowedOEM" -Type Dword -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Name "WiFISenseAllowed" -Type Dword -Value 0
+}
+
+# Disable SmartScreen filter.
+Function Disable-SmartScreen {
+    If (Confirm-DisableWithUser "SmartScreen") {
+        Return
+    }
+
+	Write-Output "Disabling SmartScreen Filter..."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Name "EnabledV9" -Type DWord -Value 0
+}
+
+# Disable Web Search in Start Menu.
+Function Disable-WebSearch {
+    If (Confirm-DisableWithUser "Bing Search in Start Menu") {
+        Return
+    }
+
+	Write-Output "Disabling Bing Search in Start Menu..."
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
+}
+
+# Disable Application suggestions and automatic installation.
+Function Disable-AppSuggestions {
+    If (Confirm-DisableWithUser "application suggestions and automatic installation") {
+        Return
+    }
+
+	Write-Output "Disabling Application suggestions..."
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "ContentDeliveryAllowed" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "OemPreInstalledAppsEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "PreInstalledAppsEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "PreInstalledAppsEverEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SilentInstalledAppsEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338387Enabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338388Enabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338389Enabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-353698Enabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SystemPaneSuggestionsEnabled" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -Type DWord -Value 1
+}
+
+# Disable Activity History feed in Task View.
+# Note: The checkbox "Let Windows collect my activities from this PC" remains checked even when the function is disabled.
+Function Disable-ActivityHistory {
+    If (Confirm-DisableWithUser "Activity History feed in Task View") {
+        Return
+    }
+
+	Write-Output "Disabling Activity History..."
+    Write-Output "Note: The checkbox 'Let Windows collect my activities from this PC' remains checked even when the function is disabled."
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableActivityFeed" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "PublishUserActivities" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "UploadUserActivities" -Type DWord -Value 0
+}
+
+
+# Disable Location Tracking.
+Function Disable-LocationTracking {
+    If (Confirm-DisableWithUser "location tracking") {
+        Return
+    }
+
+	Write-Output "Disabling Location Tracking..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type String -Value "Deny"
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration" -Name "Status" -Type DWord -Value 0
+}
+
+# Disable automatic Maps updates.
+Function Disable-AutoMapUpdates {
+    If (Confirm-DisableWithUser "automatic Maps updates") {
+        Return
+    }
+
+	Write-Output "Disabling automatic Maps updates..."
+	Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name "AutoUpdateEnabled" -Type DWord -Value 0
+}
+
+# Disable Feedback.
+Function Disable-UserFeedback {
+    If (Confirm-DisableWithUser "user feedback") {
+        Return
+    }
+
+	Write-Output "Disabling Feedback..."
+	If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules")) {
+		New-Item -Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "DoNotShowFeedbackNotifications" -Type DWord -Value 1
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClient" -ErrorAction SilentlyContinue | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" -ErrorAction SilentlyContinue | Out-Null
+}
+
+# Disable Tailored Experiences
+Function Disable-TailoredExperiences {
+    If (Confirm-DisableWithUser "tailored experiences") {
+        Return
+    }
+
+	Write-Output "Disabling Tailored Experiences..."
+	If (!(Test-Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
+		New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableTailoredExperiencesWithDiagnosticData" -Type DWord -Value 1
+}
+
+# Disable Advertising ID
+Function Disable-AdvertisingID {
+    If (Confirm-DisableWithUser "Advertising ID") {
+        Return
+    }
+
+	Write-Output "Disabling Advertising ID..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" -Name "DisabledByGroupPolicy" -Type DWord -Value 1
+}
+
+# Disable Cortana
+Function Disable-Cortana {
+    If (Confirm-DisableWithUser "Cortana") {
+        Return
+    }
+
+	Write-Output "Disabling Cortana..."
+	If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
+		New-Item -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+	If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
+		New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+	If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
+		New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+}
+
+# Restrict Windows Update P2P only to local network - Needed only for 1507 as local P2P is the default since 1511
+Function SetP2PUpdateLocal {
+    If (Confirm-DisableWithUser "Windows P2P update via internet") {
+        Return
+    }
+
+	Write-Output "Restricting Windows Update P2P only to local network..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DODownloadMode" -Type DWord -Value 1
+}
+
+# Stop and disable Diagnostics Tracking Service
+Function Disable-DiagnosticsTracking {
+    If (Confirm-DisableWithUser "Diagnostics Tracking Service") {
+        Return
+    }
+
+	Write-Output "Stopping and disabling Diagnostics Tracking Service..."
+	Stop-Service "DiagTrack" -WarningAction SilentlyContinue
+	Set-Service "DiagTrack" -StartupType Disabled
+}
+
+# Stop and disable WAP Push Service
+Function Disable-WAPPush {
+    If (Confirm-DisableWithUser "WAP Push Service") {
+        Return
+    }
+
+	Write-Output "Stopping and disabling WAP Push Service..."
+	Stop-Service "dmwappushservice" -WarningAction SilentlyContinue
+	Set-Service "dmwappushservice" -StartupType Disabled
+}
+
+# Executes everything in order.
+Write-Introduction
+Write-Ouput "Let's start by disabling the spying and privacy-invading stuff..."
+Disable-Telemetry
+Disable-WiFiSense
+Disable-SmartScreen
+Disable-WebSearch
+Disable-AppSuggestions
+Disable-ActivityHistory
+Disable-LocationTracking
+Disable-AutoMapUpdates
+Disable-UserFeedback
+Disable-TailoredExperiences
+Disable-AdvertisingID
+Disable-Cortana
+Set-P2PUpdateLocal
+Disable-DiagnosticsTracking
+Disable-WAPPush
