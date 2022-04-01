038c6b09feab3d14cfae7cb139dafe8aadd0ac69
Nathan Campos <nathan@innoveworkshop.com>
2022-04-01 18:03:51 +0100

    Added a handy alias for running background tasks

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,7 @@
 # Make sure we don't conflict with the proper wget command.
 Remove-Item Alias:wget
+
+# Handy little alias to use as an alternative to bash's &
+Function bg() {
+    Start-Process -NoNewWindow @args
+}
