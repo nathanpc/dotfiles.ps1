d8dc6f0001d045e183e47e16f8ab94d2f15d052a
Nathan Campos <nathan@innoveworkshop.com>
2022-07-24 16:55:40 +0100

    Added simple number conversion functions ToHex and ToBin to the profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -77,3 +77,39 @@ Function bg() {
 Function emacs() {
 	bg emacs @args
 }
+
+<#
+.SYNOPSIS
+Converts any integer to its hexadecimal representation in a string.
+
+.PARAMETER Number
+Integer that we wish to convert.
+
+.OUTPUTS
+Hexadecimal representation of the number in a string.
+#>
+Function ToHex() {
+    Param(
+        [Int64]$Number
+    )
+
+    "0x{0:X2}" -f $Number
+}
+
+<#
+.SYNOPSIS
+Converts any integer to its binary representation in a string.
+
+.PARAMETER Number
+Integer that we wish to convert.
+
+.OUTPUTS
+Binary representation of the number in a string.
+#>
+Function ToBin() {
+    Param(
+        [Int64]$Number
+    )
+
+    "0b" + [System.Convert]::ToString($Number, 2)
+}
